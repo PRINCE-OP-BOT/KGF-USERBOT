fe9efa032b0712b5efdbf5e5099b3c154361fc3c
PRINCE <90555516+PRINCE-OP-BOT@users.noreply.github.com>
2021-09-19 18:07:36 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,10 +1,15 @@
 import os
 
 ENV = bool(os.environ.get("ENV", False))
-
 if ENV:
-    from sample_config import Var
+    from heroku_config import Var as Config
 else:
-    if os.path.exists("config.py"):
-        from config import Development as Var
-© 2021 GitHub, Inc.
+    from local_config import Development as Config
+
+
+Var = Config
+
+
+
+#fk uuh...
+#keep spying..
