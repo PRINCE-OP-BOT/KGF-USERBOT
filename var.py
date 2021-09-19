3c50dc1f2ea145a58a97dc6bf20e6ec2588a6f7f
PRINCE <90555516+PRINCE-OP-BOT@users.noreply.github.com>
2021-09-19 18:02:17 +0530

    Create var.py

---
+++ var.py
@@ -0,0 +1,10 @@
+import os
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    from sample_config import Var
+else:
+    if os.path.exists("config.py"):
+        from config import Development as Var
+© 2021 GitHub, Inc.
